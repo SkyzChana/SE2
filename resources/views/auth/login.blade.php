2aa9be4176edd695f25f5ea1924d509d4b4697a8
SkyzChana <kayminerlll@gmail.com>
2023-09-28 16:48:54 +0700

    .x

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,6 +97,7 @@
                     </div>
                     <div class="mb-3">
                       <input type="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password" name="password" required>
+                      {{ $errors }}
                     </div>
                     <div class="form-check form-switch">
                         <div class="block mt-4">
@@ -107,15 +108,19 @@
                         </div>
                     </div>
                     <div class="text-center">
-                      <button type="button" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
+                      <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Login</button>
                     </div>
                   </form>
                 </div>
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                   <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                    @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
                   </p>
+
                 </div>
               </div>
             </div>
